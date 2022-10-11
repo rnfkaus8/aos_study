256f2040f38879bb14a2f6386b04754609283ead
rnfkaus8 <kth4350@gmail.com>
2022-10-11 20:36:58 +0900

    refac : 성능 최적화를 위해 rollButton onClick 시 ImageView 생성되던 코드에서 MainActivity onCreate 에서 diceImage: ImageView 가 생성되게 함

--- diceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ diceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -29,8 +34,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        val diceImage: ImageView = findViewById(R.id.dice_image)
 
         diceImage.setImageResource(drawableResource)
     }
